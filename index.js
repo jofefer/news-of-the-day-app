6684e79c1815587eec4c1c316e224e188d74f584
jofefer <josefemeniaferrer@gmail.com>
2018-08-15 18:20:01 +1000

    news 1.0
    just a simple JS news app

---
+++ index.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  var url = 'http://hn.algolia.com/api/v1/search?query=javascript';
+  $.getJSON(url,function(data){
+
+
+    var quotes = data.hits; //it contains 20 hits
+    var currentQuote = '';
+
+
+      $('.read').unbind('click').on('click',function(){
+          //first create a random number to show a random new (bwetneen 1- lengh)
+        var ranNum = Math.floor(Math.random() * quotes.length);
+        //select the random new
+        currentQuote = quotes[ranNum];
+        console.log(currentQuote);
+
+        //put the title and author into the html
+        $(".quoteBody").html(currentQuote.title);
+        $(".quoteAuthor").html(currentQuote.author);
+
+        //click the twitt button and open a new tap with the new
+        $(".tweetQuote").attr('href','https://twitter.com/intent/tweet?text= '
+                            + currentQuote.title + ' By- '+ currentQuote.author)
+                            .attr('target','_blank')
+                            .attr('disabled', false);
+
+        $('.read').html('Showme one more');
+
+      });
+
+
+  });
+});
